0dac3d47d3f2dc0bb00e017a4af384e1c6fca7fa
Meron Ogbai <okbaymeron@gmail.com>
2021-01-24 04:53:15 +0300

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
 module.exports = {
-  mode: 'production',
+  mode,
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -23,6 +25,10 @@ module.exports = {
         test: /\.css$/,
         include: path.resolve(__dirname, 'src'),
         use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)/i,
+        use: 'url-loader',
       },
     ],
   },
