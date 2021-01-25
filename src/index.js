ae5875277b44a176740890dafa41819054fb2d1d
Meron Ogbai <okbaymeron@gmail.com>
2021-01-25 22:49:43 +0300

    Add modals for each project

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import './style.css';
-import '../node_modules/@fortawesome/fontawesome-free/js/all';
+import '@fortawesome/fontawesome-free/js/all';
 
-const modalBtn = document.querySelector('.modal-btn');
+const modalBtns = document.querySelectorAll('.modal-btn');
 
-modalBtn.addEventListener('click', () => {
-  const modalBg = document.createElement('div');
-  const modalBody = modalBtn.parentElement.querySelector('div.project');
-  const modalFoot = modalBtn.parentElement.querySelector('ul.project-skills');
-  modalBg.classList = 'fixed inset-0 bg-gray-100 bg-opacity-70 flex justify-center items-center';
-  const modal = document.createElement('div');
-  modal.classList = 'bg-white w-64 p-5 rounded';
-  const closeBtn = document.createElement('i');
-  closeBtn.classList = 'fas fa-window-close fa-lg text-blue-800';
-  modal.append(closeBtn, modalBody.cloneNode(true), modalFoot.cloneNode(true));
-  modalBg.append(modal);
-  modalBg.addEventListener('click', () => {
-    modalBg.remove();
+modalBtns.forEach(modalBtn => {
+  modalBtn.addEventListener('click', () => {
+    const modalBg = document.createElement('div');
+    modalBg.classList = 'fixed inset-0 bg-gray-200 bg-opacity-70 flex justify-center items-center';
+    const modal = document.createElement('div');
+    modal.classList = 'bg-white w-10/12 px-3 rounded relative';
+    const modalBody = modalBtn.parentElement.querySelector('div.project');
+    const modalSkills = modalBtn.parentElement.querySelector('ul.project-skills');
+    const closeBtn = document.createElement('button');
+    closeBtn.innerHTML = '<i class="fas fa-window-close fa-lg text-blue-800 absolute top-0 right-0"></i>';
+    closeBtn.addEventListener('click', () => {
+      modalBg.remove();
+    });
+    modal.append(closeBtn, modalBody.cloneNode(true), modalSkills.cloneNode(true));
+    modalBg.append(modal);
+    modalBg.addEventListener('click', e => {
+      if (e.target === modalBg) {
+        modalBg.remove();
+      }
+    });
+    document.body.append(modalBg);
   });
-  document.body.append(modalBg);
 });
