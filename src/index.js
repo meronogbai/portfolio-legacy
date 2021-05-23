b776184bb4147a92b92e3265d58a2995b8628bf1
Meron Ogbai <okbaymeron@gmail.com>
2021-05-23 23:12:47 +0300

    Remove unnecessary js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import addShadowOnScroll from './utils/addShadowOnScroll';
 import fetchRepos from './api/fetchRepos';
 import displayRepos from './components/displayRepos';
 import './style.css';
 
-const nav = document.querySelector('nav');
 const projects = document.querySelector('#projects');
-
-addShadowOnScroll(nav);
 
 fetchRepos()
   .then(repos => {
